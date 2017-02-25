bc88bcd97d2087b57b5f5d8298cd5e8816aeb3de
ojbr <mail@octo.im>
2017-02-25 14:23:59 +0000

    HandshakeCodec unit tests

---
+++ network/src/test/java/im/octo/jungletree/network/codec/handshake/HandshakeCodecTest.java
@@ -0,0 +1,69 @@
+package im.octo.jungletree.network.codec.handshake;
+
+import im.octo.jungletree.api.GameVersion;
+import im.octo.jungletree.network.message.handshake.HandshakeMessage;
+import im.octo.jungletree.network.protocol.HandshakeState;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.flowpowered.network.util.ByteBufUtils.readUTF8;
+import static com.flowpowered.network.util.ByteBufUtils.readVarInt;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(PowerMockRunner.class)
+public class HandshakeCodecTest {
+
+    private static final Logger log = LoggerFactory.getLogger(HandshakeCodecTest.class);
+
+    private static final GameVersion GAME_VERSION = GameVersion.MC_1_11_2;
+    private static final String ADDRESS = "localhost";
+    private static final int PORT = 25565;
+    private static final HandshakeState STATE = HandshakeState.STATUS;
+
+    private ByteBuf buf;
+    private HandshakeCodec subject;
+
+    @Before
+    public void setUp() throws Exception {
+        buf = ByteBufAllocator.DEFAULT.buffer();
+        this.subject = new HandshakeCodec();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.buf = null;
+        this.subject = null;
+    }
+
+    @Test
+    public void decode() throws Exception {
+        // Given
+        HandshakeMessage message = new HandshakeMessage(GAME_VERSION.getProtocolVersion(), ADDRESS, PORT, STATE.getId());
+        subject.encode(buf, message);
+
+        // When
+        HandshakeMessage result = subject.decode(buf);
+
+        // Then
+        assertEquals(message, result);
+    }
+
+    @Test
+    public void encode() throws Exception {
+        // When
+        subject.encode(buf, new HandshakeMessage(GAME_VERSION.getProtocolVersion(), ADDRESS, PORT, STATE.getId()));
+
+        // Then
+        assertEquals(GAME_VERSION.getProtocolVersion(), readVarInt(buf));
+        assertEquals(ADDRESS, readUTF8(buf));
+        assertEquals(PORT, buf.readUnsignedShort());
+        assertEquals(STATE.getId(), readVarInt(buf));
+    }
+}
