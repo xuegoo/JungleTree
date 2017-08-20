c035c39d121fbc357470a5e2f86a81c7b1e4ffad
ojbr <mail@octo.im>
2017-08-20 16:21:05 +0100

    Configurable Redis host address

--- jungletree-storage/src/main/java/org/jungletree/storage/storage/JungleStorageService.java
+++ jungletree-storage/src/main/java/org/jungletree/storage/storage/JungleStorageService.java
@@ -3,16 +3,21 @@ package org.jungletree.storage.storage;
 import org.jungletree.rainforest.storage.StorageService;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class JungleStorageService implements StorageService {
 
+    private static final String REDIS_HOST = System.getenv("REDIS_HOST") != null ? System.getenv("REDIS_HOST") : "localhost";
+
     private final RedissonClient client;
 
     public JungleStorageService() {
-        this.client = Redisson.create();
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + REDIS_HOST + ":6379");
+        this.client = Redisson.create(config);
     }
 
     @Override
